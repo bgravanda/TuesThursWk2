34eeb456ec58702aae9538191d6cdd12016fe63a
bgravanda <bgravanda@student.bridgew.edu>
2021-09-07 10:46:19 -0400

    Example3

---
+++ src/Example3.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Example3 {
+    public static void main(String[] args){
+        var studentNames = new ArrayList<String>();
+        var reader = new Scanner(System.in);
+        var userResponce = "";
+        while(!userResponce.equals("done")){
+            System.out.print("Enter next mane or 'done' to end:");
+            userResponce = reader.nextLine();
+            if (!userResponce.equals("done")){
+                studentNames.add(userResponce);
+
+            }
+        }
+    }
+}
