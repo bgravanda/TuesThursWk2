736679cae882975ef08c7cba0cec93c11d85086d
bgravanda <bgravanda@student.bridgew.edu>
2021-09-07 10:17:44 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	    var reader = new Scanner(System.in);
+        System.out.print("How old are you?");
+        var age = reader.nextFloat();
+        var dogyears = age*7;
+        System.out.println("Wow you are "+age+7" that is " +dogyears+ " in dog year");
+    }
+}
